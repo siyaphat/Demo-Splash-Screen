b1c0a2e490903ef273221fc964922906511fdfba
siyaphat <siyaphat.ple@gmail.com>
2020-04-21 15:31:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/demo_splash_screen/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.demo_splash_screen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(HomeActivity.this
+                        , MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
